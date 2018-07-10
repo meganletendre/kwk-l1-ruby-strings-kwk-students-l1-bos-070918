0e471a49ca519c2a5168b9afac76282e7dec4b07
meganletendre <megan.letendre@comcast.net>
2018-07-10 14:16:40 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -21,7 +21,7 @@ puts "What is the host's name?"
 
 host_name = gets.chomp
 
-"Dear #{guest_name},
+puts "Dear #{guest_name},
 You are cordially invited to the #{party_name} on #{date} at #{time} . Please RSVP no later than October 30.
 
 Sincerely,
